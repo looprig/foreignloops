b46a9c576f8cbc064957c188c76da4c4e83e57f4
Krishna C <github@kumb.uk>
2026-08-11 09:17:19 -0400

    chore(deps): adopt final runtime patch train

--- backend/steering_integration_test.go
+++ backend/steering_integration_test.go
@@ -481,7 +481,7 @@ func TestSteeringIntegrationInjectedFoldCheckedBeforeTerminal(t *testing.T) {
 }
 
 func TestSteeringIntegrationPromptRequiredQueuesExactlyOneFallback(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	t.Cleanup(cancel)
 	script := steertest.DefaultScript()
 	script.Prompts = []steertest.PromptScript{
